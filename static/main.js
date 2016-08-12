bd903835326f6a834f7cee6385b09735f19ffc6d
Ujq151 <samuel.moore@capitalone.com>
2016-08-12 12:06:10 -0400

    change index and main js files

--- static/main.js
+++ static/main.js
@@ -5,7 +5,24 @@ socket.onopen = function () {
 	console.log('Web socket has opened');
 };
 
+socket.onclosed = function () {
+	console.log('Web socket has closed');
+};
+
 socket.onmessage = function (event) {
   console.log(event.data);
 }
 
+document.getElementById("message")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+    	var message = document.getElementById("message").value;
+    	document.getElementById("message").value = "";
+    	processInput(message)
+    }
+});
+
+function processInput(message) {
+	socket.send(message);
+}
